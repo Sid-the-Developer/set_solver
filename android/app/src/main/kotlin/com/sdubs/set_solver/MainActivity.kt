68b36590576d0170c591a090d8b9ae65e6043cf0
Sid-the-Developer <swright3743@gmail.com>
2022-01-18 19:02:42 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/sdubs/set_solver/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sdubs.set_solver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/board_analyzer.dart
@@ -0,0 +1,3 @@
+public class BoardAnalyzer {
+  private List[]
+}
